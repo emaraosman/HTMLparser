149e709d8f5ae47eb772ef90fa04884b3d026989
Osman Emara <emaraosman@gmail.com>
2018-01-02 19:32:51 -0500

    create migration

--- htmlparser/db/migrate/20180103003113_create_posts.rb
+++ htmlparser/db/migrate/20180103003113_create_posts.rb
@@ -1,7 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-
+      t.string :urls
+      t.string :h1
+      t.string :h2
+      t.string :h3
+      t.string :links
       t.timestamps
     end
   end
